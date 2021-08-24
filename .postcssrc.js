b6a74b972e89f0cc0ef898f93401876c7c6b347e
jjw7795 <jjw7795@naver.com>
2021-08-24 22:02:19 +0900

    create project

---
+++ .postcssrc.js
@@ -0,0 +1,6 @@
+//import autoprefixer from 'autoprefixer'
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  Plugins: [autoprefixer],
+};
---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+async function test() {
+  const promise = Promise.resolve(123);
+
+  console.log(await promise);
+  console.log(promise);
+}
+test();
